b0da957b15c089b8d41c6e7809b88eb1555ee4a3
Hannah Frost <h.7frost@gmail.com>
2019-04-26 16:48:32 +0100

    Displays bookmarks as links

--- spec/features/bookmark_managing_spec.rb
+++ spec/features/bookmark_managing_spec.rb
@@ -1,9 +1,10 @@
 feature 'changes the bookmarks in the manager' do
   scenario 'a user can add a bookmark' do
     visit('/bookmarks/new')
-    fill_in 'ws_url', with: 'http://www.makersacademy.com'
+    fill_in 'title', with: 'Makers Academy'
+    fill_in 'url', with: 'http://www.makersacademy.com'
     click_button 'Add Website'
 
-    expect(page).to have_content 'http://www.makersacademy.com'
+    expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
   end
 end
--- spec/units/bookmark_spec.rb
+++ spec/units/bookmark_spec.rb
@@ -1,25 +1,33 @@
 require 'bookmark'
+require 'database_helpers'
 
 describe Bookmark do
   describe '#all' do
     it 'displays all bookmarks' do
       connection = PG.connect(dbname: 'bookmark_manager_test')
 
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+      bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+      Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
+      Bookmark.create(url: "http://www.google.com", title: "Google")
 
       bookmark_list = Bookmark.all
 
-      expect(bookmark_list).to include "http://www.makersacademy.com"
-      expect(bookmark_list).to include "http://www.destroyallsoftware.com"
-      expect(bookmark_list).to include "http://www.google.com"
+      expect(bookmark_list.length).to eq 3
+      expect(bookmark_list.first).to be_a Bookmark
+      expect(bookmark_list.first.id).to eq bookmark.id
+      expect(bookmark_list.first.title).to eq "Makers Academy"
+      expect(bookmark_list.first.url).to eq "http://www.makersacademy.com"
     end
   end
   describe '#create' do
     it 'adds a bookmark to the database' do
-      Bookmark.create(url: 'http://www.makersacademy.com')
-      expect(Bookmark.all).to include 'http://www.makersacademy.com'
+      bookmark = Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+      persisted_data = persisted_data(id: bookmark.id)
+
+      expect(bookmark).to be_a Bookmark
+      expect(bookmark.id).to eq bookmark.id
+      expect(bookmark.url).to eq 'http://www.makersacademy.com'
+      expect(bookmark.title).to eq 'Makers Academy'
     end
   end
 end
